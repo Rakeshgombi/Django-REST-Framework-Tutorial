c161a86b0cb0f0f98efc4f9350e930ff099f7ea2
Rakesh Gombi <rakeshgombi18@gmail.com>
2022-08-29 01:27:08 +0530

    requesting django localhost using python requests

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,13 +1,15 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
 
-get_response = requests.get(
-    endpoint, json={"query": "Hello World"})  # Http Request
-# print(get_response.text)  # print raw text response code
+get_response = requests.get(endpoint,
+                            json={"query": "Hello World"}
+                            )  # Http Request
+print(get_response.text)  # print raw text response code
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
-print(get_response.json())
+# print(get_response.json())
 print(get_response.status_code)
