cbbc7086de56d782e673b21db804f1393f493ca8
Rakesh Gombi <rakeshgombi18@gmail.com>
2022-08-29 18:57:08 +0530

    Django Model instance as a API Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,24 +1,20 @@
 import json
+
 from django.http import JsonResponse
+from products.models import Product
 
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
-    # request -> HttpRequest -> Django
-    # print(dir(request))
-    # request.body
-    print(request.GET)  # URL query parameters
-    # print(request.POST)   # URL query parameters
-    body = request.body  # Byte string of JSON Data
+    model_data = Product.objects.all().order_by('?').first()
+    # print(model_data)
     data = {}
-    try:
-        # Converting the JSON data into a Python dictionary.
-        data = json.loads(body)
-    except:
-        pass
-    # print(data)
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)   # request.META
-    # print(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        # model instance (model_data)
+        # Turn a Python dict
+        # return JSON to my client
     return JsonResponse(data)
