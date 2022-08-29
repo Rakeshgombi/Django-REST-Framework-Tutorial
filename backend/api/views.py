c441335dca8b7b545c3db4c95a530ebabaaf057d
Rakesh Gombi <rakeshgombi18@gmail.com>
2022-08-29 17:22:47 +0530

    Echo GET Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,24 @@
+import json
 from django.http import JsonResponse
 
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!"})
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    # request.body
+    print(request.GET)  # URL query parameters
+    # print(request.POST)   # URL query parameters
+    body = request.body  # Byte string of JSON Data
+    data = {}
+    try:
+        # Converting the JSON data into a Python dictionary.
+        data = json.loads(body)
+    except:
+        pass
+    # print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)   # request.META
+    # print(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,11 +5,12 @@ import requests
 endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint,
+                            params={"abc": 123},
                             json={"query": "Hello World"}
                             )  # Http Request
 # print(get_response.text)   # print raw text response code
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
-print(get_response.json()['message'])
+print(get_response.json())
 print(get_response.status_code)
