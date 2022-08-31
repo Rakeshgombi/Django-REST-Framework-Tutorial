0b62f841d336e59ed74eeb4477dcfb47f4e2b831
Rakesh Gombi <rakeshgombi18@gmail.com>
2022-08-31 13:28:21 +0530

    Django Rest Framework Model Serializer

---
+++ backend/products/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Product
+
+
+class ProductForm(forms.Model):
+    class Meta:
+        model = Product
+        fields = [
+            "title",
+            "content",
+            "price",
+        ]
---
+++ backend/products/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from .models import Product
+
+
+class PrimaryProductSerializers(serializers.ModelSerializer):
+    discount = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            "title",
+            "content",
+            "price",
+            'sale_price',
+            'discount',
+        ]
+
+    def get_discount(self, obj):
+        print(obj.id)
+        return obj.get_discount()
